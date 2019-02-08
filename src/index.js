e8e2447dd468967d1eb8518fe8d9aa4129ceefc7
LannyWang009 <09legend.wang@gmail.com>
2019-02-07 21:25:31 -0600

    change color works; switched to immutable functions through immutable.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
+
+// ----------
+// Util
+// ----------
+const byId = (id) => document.getElementById(id)
+const squigglyLine = () => console.log('~~~~~~~~~~~~~~~~~~~~~')
+const deepCopy = (x) => JSON.parse(JSON.stringify(x))
+
+// ==============================================================
+// RENDER
+
+const counterEl = byId('counterValue')
+const render = () => {
+  const currentState = getState()
+  counterEl.innerHTML = Immutable.get(currentState, 'counter')
+  counterEl.style.color = Immutable.get(currentState, 'color')
+}
+
+// ==============================================================
+// Initial Value and Reducer
+const initialValue = Immutable.fromJS({
+  color: 'blue',
+  counter: 0
+})
+
+const reducer = (currentState = initialValue, action) => {
+  // to write an error message if the assertion is false
+  console.assert(Immutable.isImmutable(currentState), 'Redux store should be an immutable value.')
+
+  let nextState = currentState
+  let { type } = action
+  //  let type = action.type
+  if (type === 'INCREMENT') {
+    console.log('action', action)
+    nextState = Immutable.updateIn(currentState, ['counter'], (c) => c + action.value)
+  } else if (type === 'CHANGECOLOR') {
+    console.log('action', action)
+    nextState = Immutable.set(currentState, 'color', action.color)
+  } return nextState
+}
+
+// ==============================================================
+// Create Redux Store
 const { createStore } = Redux
 
 // Initialize the Redux store by passing it our reducer (defined globally in reducer.js)
@@ -5,26 +48,41 @@ const { subscribe, dispatch, getState } = createStore(reducer)
 
 // Re-render the application every time the state changes
 // Here we pass the Redux state to our render method (defined globally in render.js)
-subscribe(() => render(getState()))
+subscribe(render)
 
+// dispatch an empty 'INIT' event to trigger the first render
+dispatch({ type: 'INIT' })
+
+// ==============================================================
+// DOM EVENTS
+const addEvents = () => {
 // Dispatch the "INCREMENT" action every time the +1 button is pressed
-const increment1Button = document.getElementById('increment1')
-increment1Button.addEventListener('click', e => dispatch({ type: 'INCREMENT', value: 1 }))
+  const increment1Button = document.getElementById('increment1')
+  increment1Button.addEventListener('click', e => dispatch({ type: 'INCREMENT', value: 1 }))
 
-// Dispatch the "INCREMENT" action every time the -1 button is pressed
-const decrement1Button = document.getElementById('decrement1')
-decrement1Button.addEventListener('click', e => dispatch({ type: 'INCREMENT', value: -1 }))
+  // Dispatch the "INCREMENT" action every time the -1 button is pressed
+  const decrement1Button = document.getElementById('decrement1')
+  decrement1Button.addEventListener('click', e => dispatch({ type: 'INCREMENT', value: -1 }))
 
-// Dispatch the "INCREMENT" action every time the +5 button is pressed
-const increment5Button = document.getElementById('increment5')
-increment5Button.addEventListener('click', e => dispatch({ type: 'INCREMENT', value: 5 }))
+  // Dispatch the "INCREMENT" action every time the +5 button is pressed
+  const increment5Button = document.getElementById('increment5')
+  increment5Button.addEventListener('click', e => dispatch({ type: 'INCREMENT', value: 5 }))
 
-// Dispatch the "INCREMENT" action every time the -5 button is pressed
-const decrement5Button = document.getElementById('decrement5')
-decrement5Button.addEventListener('click', e => dispatch({ type: 'INCREMENT', value: -5 }))
+  // Dispatch the "INCREMENT" action every time the -5 button is pressed
+  const decrement5Button = document.getElementById('decrement5')
+  decrement5Button.addEventListener('click', e => dispatch({ type: 'INCREMENT', value: -5 }))
 
-// Dispatch the "CHANGECOLOR" action every time the color is submitted
-const colorButton = document.getElementById('colorform')
-const colorSubmit = document.getElementById('colorSubmit')
-colorSubmit.addEventListener('click', e=> console.log())
-console.log(selectedColor.value)
+  // Dispatch the "CHANGECOLOR" action every time the color is submitted
+  const colorForm = document.getElementById('colorForm')
+
+  colorForm.addEventListener('submit', e => {
+    let formData = new FormData(colorForm)
+    e.preventDefault()
+    console.log('you submitted the form')
+    let newColor = formData.get('color')
+    console.log(newColor)
+    dispatch({ type: 'CHANGECOLOR', color: newColor })
+  })
+}
+
+addEvents()
--- src/reducer.js
+++ src/reducer.js
@@ -1,16 +1,22 @@
-const initialState = {
-  value: 0
-}
+// const initialState = {
+//   value: 0
+// }
 
-const reducer = (state = initialState, action) => {
-  let { type } = action
-  //  let type = action.type
-  // let { value } = action
-  if (type === 'INCREMENT') {
-    console.log('action', action)
-    return {
-      value: state.value + action.value
-    }
-  }
-  return state
-}
+// const reducer = (state = initialState, action) => {
+//   let { type } = action
+//   //  let type = action.type
+//   // let { value } = action
+//   if (type === 'INCREMENT') {
+//     console.log('action', action)
+//     return {
+//       value: state.value + action.value
+//     }
+//   }
+//   if (type === 'CHANGECOLOR') {
+//     console.log('action', action)
+//     return {
+//       color: action.color
+//     }
+//   }
+//   return state
+// }
